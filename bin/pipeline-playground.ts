ddf7046c523f8a2ca2604d8551c0fd88ac246636
Antonio Guillen <aguillen@securecarecorp.com>
2024-10-24 15:13:15 -0500

    messing with account issue

--- bin/pipeline-playground.ts
+++ bin/pipeline-playground.ts
@@ -11,11 +11,13 @@ new PipelinePlaygroundStack(app, 'PipelinePlaygroundStack', {
 
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  //env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  //env: { account: '851725654357', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
+
+app.synth();
--- lib/pipeline-playground-stack.ts
+++ lib/pipeline-playground-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -6,11 +7,15 @@ export class PipelinePlaygroundStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'PipelinePlaygroundQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new CodePipeline(this, 'PracticePipeline', {
+      pipelineName: 'PracticePipeline',
+      synth: new ShellStep('Synth', {
+        input : CodePipelineSource.gitHub('aguillen-SCC/Pipeline-Playground', 'main'),
+        commands: [
+          'npm ci',
+          'npx cdk synth'
+        ]
+      })
+    })
   }
 }
