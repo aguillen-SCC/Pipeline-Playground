4f6f905385c4381dc74a7d6dd6a4e240bc14bc29
Antonio Guillen <aguillen@securecarecorp.com>
2024-11-20 10:16:20 -0600

    added lambda function

--- lib/LamdaStack.ts
+++ lib/LamdaStack.ts
@@ -1,5 +1,8 @@
 import { Stack, StackProps } from "aws-cdk-lib";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from "constructs";
+import { join } from "path";
 
 interface LamdaStackProps extends StackProps {
     stageName?: string
@@ -8,6 +11,15 @@ export class LamdaStack extends Stack {
 
     constructor(scope: Construct, id: string, props: LamdaStackProps) {
         super(scope, id, props)
+
+        new NodejsFunction(this, 'hello-lambda', {
+            runtime: Runtime.NODEJS_20_X,
+            handler: 'handler',
+            entry: (join(__dirname,  '..', 'services', 'hello.ts')),
+            environment: {
+                STAGE: props.stageName!
+            }
+        })
     }
 
 }
